558303af09e927f1c87c3bd3e6f0eba184ef4e4c
aaneitchik <aaneitchik@gmail.com>
2017-04-11 22:01:00 +0300

    Add some dependencies, start webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
 
+const DEVELOPMENT = process.env.NODE_ENV === 'development';
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+const entry = PRODUCTION
+    ? './src/index.js'
+    : [
+          './src/index.js',
+          'webpack/hot/dev-server',
+          'webpack-dev-server/client?http://localhost:3000'
+      ];
+
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.join(__dirname, 'dist'),
-		publicPath: '/dist/',
-		filename: 'bundle.js'
+    entry,
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loaders: ['babel-loader'],
+				exclude: /node_modules/
+			}
+		]
 	}
 };
