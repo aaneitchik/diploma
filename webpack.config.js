8e844e79b67469bd0dbebd9705adcbb70da2b25c
Ann Aneychik <aaneitchik@gmail.com>
2017-05-26 14:02:18 +0300

    YAY! Setup production mode

--- webpack.config.js
+++ webpack.config.js
@@ -28,16 +28,38 @@ const plugins = PRODUCTION
 				minChunks: Infinity,
 				filename: '[name].[hash].js'
 			}),
+			new webpack.DefinePlugin({
+				'process.env': {
+					NODE_ENV: JSON.stringify('production'),
+				},
+			}),
+			new webpack.optimize.UglifyJsPlugin({
+				compress: {
+					warnings: false,
+					screw_ie8: true,
+					conditionals: true,
+					unused: true,
+					comparisons: true,
+					sequences: true,
+					dead_code: true,
+					evaluate: true,
+					if_return: true,
+					join_vars: true,
+				},
+				output: {
+					comments: false,
+				},
+			}),
 			new ExtractTextPlugin('styles.css'),
 			new HTMLWebpackPlugin({
 				template: 'index-template.ejs',
 				inject: false
-			})
+			}),
+			new BundleAnalyzerPlugin(),
 		]
 	: [
 			new webpack.HotModuleReplacementPlugin(),
 			new webpack.LoaderOptionsPlugin(),
-			new BundleAnalyzerPlugin(),
 			new webpack.optimize.CommonsChunkPlugin({
 				name: 'vendor',
 				minChunks: Infinity,
@@ -83,8 +105,8 @@ module.exports = {
 		publicPath: '/',
 		filename: '[name].[hash].js'
 	},
+	devtool: PRODUCTION ? false : 'source-map',
 	plugins,
-	devtool: 'inline-eval-cheap-source-map',
 	resolve: {
 		extensions: ['.js', '.jsx'],
 		modules: [path.resolve(__dirname, './src'), 'node_modules']
