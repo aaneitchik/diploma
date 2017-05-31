b49fa29c9f70d977107c40370cd0c289c35589f9
Ann Aneychik <aaneitchik@gmail.com>
2017-05-31 16:00:38 +0300

    Configure tag adding keys

--- src/app/components/tag-input.jsx
+++ src/app/components/tag-input.jsx
@@ -3,6 +3,8 @@ import Tags from 'react-tagging-input';
 
 import { inputShape } from '../utils/common-proptypes';
 
+const addKeys = [13, 32];
+
 class TagInput extends React.Component {
 	addTag = tag => {
 		const newTags = [...this.props.input.value, tag];
@@ -13,16 +15,26 @@ class TagInput extends React.Component {
 		newTags.splice(index, 1);
 		this.props.input.onChange(newTags);
 	};
+	stopPropagation = event => {
+		if (addKeys.includes(event.keyCode)) {
+			event.preventDefault();
+		}
+	};
 	render() {
+		/* eslint-disable jsx-a11y/no-static-element-interactions */
 		return (
-			<Tags
-				tags={this.props.input.value}
-				uniqueTags
-				onAdded={this.addTag}
-				onRemoved={this.removeTag}
-				placeholder="Добавьте тег"
-			/>
+			<span onKeyDown={this.stopPropagation}>
+				<Tags
+					tags={this.props.input.value}
+					uniqueTags
+					addKeys={addKeys}
+					onAdded={this.addTag}
+					onRemoved={this.removeTag}
+					placeholder="Добавьте тег"
+				/>
+			</span>
 		);
+		/* eslint-enable jsx-a11y/no-static-element-interactions */
 	}
 }
 
