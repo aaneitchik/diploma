fff49964c4346c1efe9f8314dbadb1f6da485977
Ann Aneychik <aaneitchik@gmail.com>
2017-06-05 09:34:43 +0300

    Update dependencies, prepare for file upload redirect

--- src/app/load-file/load-file.js
+++ src/app/load-file/load-file.js
@@ -3,14 +3,29 @@ import { toastr } from 'react-redux-toastr';
 
 const ROOT_URL = '/api/files';
 
+const TOGGLE_FILE_UPLOAD = 'load-file/TOGGLE_FILE_UPLOAD';
+
+export default function reducer(state = {}, action) {
+	switch (action.type) {
+		case TOGGLE_FILE_UPLOAD:
+			return { ...state, uploadSuccessful: action.payload };
+		default:
+			return state;
+	}
+}
+
 export function loadFile(file) {
-	return () => {
+	return dispatch => {
 		axios.post(`${ROOT_URL}/upload`, file).then(
 			() => {
-				toastr.success('', 'File uploaded successfully!');
+				dispatch({ type: TOGGLE_FILE_UPLOAD, payload: true });
+				toastr.light('', 'Файл успешно загружен!', { icon: 'success' });
 			},
 			err =>
-				toastr.error('', `An error occured while loading file: ${err}`)
+				toastr.error(
+					'',
+					`Во время загрузки фала произошла ошибка: ${err}`
+				)
 		);
 	};
 }
